f07242a29b1590b1e95e3710e75d33c91156ac05
Vincent Francolin <vincent.francolin@outlook.com>
2022-07-20 19:28:43 +0200

    Update PWA manifest

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,18 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   outputDir: "docs", // for github pages
   publicPath: "./",
+  pwa: {
+    themeColor: "purple",
+    manifestOptions: {
+      icons: [
+        {
+          src: "img/icons/app_icon.png",
+          sizes: "750x750",
+          type: "image/png",
+        },
+      ],
+    },
+  },
   transpileDependencies: [
     "vuetify",
   ],
